901f82c259bd8e8994286d91dd0bab3255003a9b
carlos <carlosdav03@gmail.com>
2025-06-10 21:56:24 -0600

    Update genetic algorithm
    Implementation of fitness function by looking for any conflicts between the queens, returns 0 when optimal

--- metaheuristics/genetic-algorithm.py
+++ metaheuristics/genetic-algorithm.py
@@ -10,9 +10,22 @@ def generar_tablero(tamano):
 # Valor preliminar
 GENERACIONES_MAXIMAS = 150
 
+# Función para calcular el fitness de un tablero, es decir comprobar si se da alguna colisión entre las reinas
+def fitness(tablero):
+    n = len(tablero)
+    colisiones = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if tablero[i] == tablero[j]:
+                ataques += 1
+            if abs(tablero[i] - tablero[j]) == abs(i - j):
+                ataques += 1
+    return colisiones
+
 def algoritmo_genetico():
     # Se crea la población de 30 tableros para cada tratamiento
     # Tamano de tablero fijo en 8, falta hacer para que cambie a 20
-    poblacion_tablero = [(generar_tablero(8), 0) for _ in range(30)]
+    poblacion_tableros = [(generar_tablero(8), 0) for _ in range(30)]
 
     for generation in range(GENERACIONES_MAXIMAS):
+        poblacion_tableros = [(tablero, fitness(tablero)) for tablero, _ in poblacion_tableros]
