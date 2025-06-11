80cdfd89851cf30cc5022a5a1fdfe73b26e1bb02
carlos <carlosdav03@gmail.com>
2025-06-10 21:08:16 -0600

    Function to generate boards
    Boards are created with queens in random positions for each column, with 'tamano' being used as a parameter which corresponds to the given size of the board.

--- metaheuristics/genetic-algorithm.py
+++ metaheuristics/genetic-algorithm.py
@@ -1,3 +1,9 @@
 # Algoritmo genético para el problema de las n-reinas
 
 import random
+
+# Genera un tablero con posiciones aleatorias
+def generar_tablero(tamano):
+    tablero = [random.randint(0, (tamano-1)) for _ in range(tamano)]
+    return tablero
+
