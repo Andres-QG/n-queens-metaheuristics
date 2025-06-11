aa4142c7c535ecfcc19dfd4b59c5cfea59f9850f
carlos <carlosdav03@gmail.com>
2025-06-10 21:24:49 -0600

    Starting genetic algorithm function implementation
    Creation of population for each treatment and for cycle for evaluation of each generation.

--- metaheuristics/genetic-algorithm.py
+++ metaheuristics/genetic-algorithm.py
@@ -7,3 +7,12 @@ def generar_tablero(tamano):
     tablero = [random.randint(0, (tamano-1)) for _ in range(tamano)]
     return tablero
 
+# Valor preliminar
+GENERACIONES_MAXIMAS = 150
+
+def algoritmo_genetico():
+    # Se crea la población de 30 tableros para cada tratamiento
+    # Tamano de tablero fijo en 8, falta hacer para que cambie a 20
+    poblacion_tablero = [(generar_tablero(8), 0) for _ in range(30)]
+
+    for generation in range(GENERACIONES_MAXIMAS):
